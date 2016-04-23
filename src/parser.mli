56c4ad2fd90dd2c4d7b75fb635bf7eab5b8689df
Davide Gessa <gessadavide@gmail.com>
2016-04-23 12:21:58 +0200

    Oasis integration

--- src/parser.mli
+++ src/parser.mli
@@ -1,5 +1,7 @@
-module Parser =
-	let contract_of_bytes -> string -> Instr.t list;;
-	let contract_to_asm -> Instr.t list -> string;;
-	let contract_to_bytes -> Instr.t list -> string;;
+open Instr;;
+
+module Parser : sig
+	val contract_of_bytes : string -> Instr.t list
+	val contract_to_asm   : Instr.t list -> string
+	val contract_to_bytes : Instr.t list -> string
 end
